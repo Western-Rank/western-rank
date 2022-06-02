e1bbabcb68ab2b3c24e38b232c3e89048fb48c7f
LordExodius <oscar45697@gmail.com>
2022-06-01 21:44:20 -0400

    replaced default _app.tsx with auth0 userprovider components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+// pages/_app.js
 import '../styles/globals.css'
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }:AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
-
-export default MyApp
