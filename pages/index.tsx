b68fa7840b1b21fb47857d86bbf317b51e52efff
LordExodius <oscar45697@gmail.com>
2022-06-01 22:29:23 -0400

    changed profile link to Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -18,7 +19,7 @@ const Home: NextPage = () => {
           <br></br>
           <a href="/api/auth/login">Login</a><br></br>
           <a href="/api/auth/logout">Logout</a><br></br>
-          <a href="/profile">Profile</a>
+          <Link href="/profile">Profile</Link>
 
         </h1>
 
