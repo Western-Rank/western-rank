86853a6bb657129f53753a3799ac433cca7bf1f0
basokant <basokanthan@gmail.com>
2023-05-10 22:06:14 -0400

    generate initial migration and schema using drizzle-kit introspective

--- lib/database/schema/reviews.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
-
-export const courses = pgTable('courses', {
-    review_id: serial('review_id').primaryKey(),
-    course_code: varchar('course_code', { length: 256 })
-        .references(() => courses.course_code),
-    professor: text('professor'),
-    
-});
