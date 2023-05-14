2e2b311868b47cecd513b71d0560d7306c1075ab
basokant <basokanthan@gmail.com>
2023-05-13 22:02:19 -0400

    remove flexsearch, use AutoComplete component's default searching, change displayed option limit to 15

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,23 +1,19 @@
 // import SearchIcon from '@mui/icons-material/Search'
 import { useRouter } from 'next/router';
-import { Autocomplete, TextField } from '@mui/material'
+import { Autocomplete, TextField, createFilterOptions } from '@mui/material'
 import styles from '../styles/Searchbar.module.scss'
 import { useState, useEffect } from 'react';
 import { debounce } from '../lib/debounce';
 import { useQuery } from '@tanstack/react-query';
-const { Index } = require("flexsearch");
-
-const course_index = Index({
-  tokenize: 'full'
-});
 
 const Searchbar = () => {
-  const [courseOptions, setCourseOptions] = useState<string[]>([]);
-  const [allCourses, setAllCourses] = useState<string[]>([]);
-  // used to navigate to new course page when selected in the search menu
   const router = useRouter();
+  const filterOptions = createFilterOptions({
+    matchFrom: 'any',
+    limit: 15
+  })
 
-  useQuery({
+  const { data, isLoading, isError }= useQuery({
     queryFn: async () => {
       const response = await fetch('/api/courses');
       if (!response.ok) {
@@ -25,46 +21,30 @@ const Searchbar = () => {
       }
       return response.json();
     },
-    onSuccess(data: string[]) {
-      console.log(data);
-      setAllCourses(data);
-      data.forEach((course: string, index: number) => {
-        course_index.add(index, course);
-      })
-    },
-    refetchOnWindowFocus: false
-  });
-
-  const onCourseSearch = debounce(async (ev: React.SyntheticEvent, value: string | null) => {
-    if (value !== null && value.trim().length !== 0) {
-      const course_index_results = course_index.search(value, 20);
-      const courses = course_index_results.map((index: number) => allCourses[index]);
-      console.log(value, courses);
-      setCourseOptions(courses);
-    } else {
-      setCourseOptions(allCourses.slice(0,20));
+    refetchOnWindowFocus: false,
+    onSuccess(data) {
+      data.sort();
     }
   });
 
-  const onCourseSelect = (ev: React.SyntheticEvent, value: string | null) => {
-    if (value === null)
+  const onCourseSelect = (ev: React.SyntheticEvent, value: unknown) => {
+    if (typeof(value) !== 'string' || value === null)
       return;
+
     // extract the course code from the full course name,
     // encoding it as a valid URI string
     // e.g. CALC 1000: Calculus I -> CALC%201000
-    const courseCodeURI = encodeURIComponent(value.split(':')[0]);
+    const courseCodeURI = encodeURIComponent(value?.split(':')[0]);
     router.push(`/course/${courseCodeURI}`);
   }
 
   return (
     <Autocomplete
-      freeSolo
       className={styles.searchbar}
-      options={courseOptions}
+      options={!isLoading && !isError ? data : []}
       renderInput={(params) => <TextField {...params}/>}
       onChange={onCourseSelect}
-      onInputChange={onCourseSearch}
-      filterOptions={(x: any) => x} // override default filtering (we filter with onCourseSearch)
+      filterOptions={filterOptions}
     />
   )
 }
