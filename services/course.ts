cd65027a6bc2fbc2a29206bbbea3519e1148ea86
Maaz Ali <maazali22@gmail.com>
2023-05-27 19:43:22 -0400

    added course services

---
+++ services/course.ts
@@ -0,0 +1,39 @@
+import { Course } from "@prisma/client"
+import { prisma } from "../lib/db"
+
+/**
+ * Search for courses stored in the database.
+ * @param query Substring to match course names/codes with
+ * @returns List of courses that match the given query
+ */
+export function searchCourses(query: string): Promise<Course[]> {
+  return prisma.course.findMany({
+    where: {
+      OR: [
+        { course_code: { contains: query } },
+        { course_name: { contains: query } },
+      ],
+    },
+  })
+}
+
+/**
+ * Get all courses stored in the database.
+ * @returns The list of courses formatted as '<COURSE_CODE>: <COURSE_NAME>'
+ */
+export function getAllCourses() {
+  return prisma.course.findMany()
+}
+
+/**
+ * Get course by course code.
+ * @param courseCode the course code of the course to fetch
+ * @returns the course information stored in the database
+ */
+export function getCourse(courseCode: string) {
+  return prisma.course.findUnique({
+    where: {
+      course_code: courseCode,
+    },
+  })
+}
