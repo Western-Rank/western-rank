f705a4f8b8880d7044b0e9c10d90c2a34959a49b
Maaz Ali <maazali22@gmail.com>
2023-05-27 17:06:32 -0400

    formatting

--- lib/database/db.ts
+++ lib/database/db.ts
@@ -1,8 +1,8 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from "drizzle-orm/node-postgres"
+import { Pool } from "pg"
 
-import { courses } from './schema';
-import { course_reviews } from './schema';
+import { courses } from "./schema"
+import { course_reviews } from "./schema"
 
 const pool = new Pool({
   host: process.env.DB_HOST_NAME,
@@ -10,7 +10,7 @@ const pool = new Pool({
   user: process.env.DB_USER_NAME,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  max: 10
-});
+  max: 10,
+})
 
-export const db = drizzle(pool);
+export const db = drizzle(pool)
