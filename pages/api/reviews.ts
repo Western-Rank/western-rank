3ed4f4c5d306fc5fd69810b28e27adaa43ea2733
Maaz Ali <maazali22@gmail.com>
2023-05-27 22:08:08 -0400

    updated /reviews to prisma

--- pages/api/reviews.ts
+++ pages/api/reviews.ts
@@ -1,16 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-import { postReview, CourseReview, deleteReview } from '../../lib/reviews';
+import type { NextApiRequest, NextApiResponse } from "next"
+import { createReview, deleteReview, upsertReview } from "../../services/review"
+import { Course_Review } from "@prisma/client"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
-    case 'DELETE': handleDeleteReview(req, res); break;
-    case 'PUT':
-    case 'POST': handlePostReview(req, res); break;
-    default: res.send('Invalid API route'); break;
+    case "DELETE":
+      handleDeleteReview(req, res)
+      break
+    case "PUT":
+    case "POST":
+      handlePostReview(req, res)
+      break
+    default:
+      res.send("Invalid API route")
+      break
   }
 }
-
 
 /**
  * Delete a review from the database.
@@ -20,27 +25,28 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
  */
 async function handleDeleteReview(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { email, course_code } = req.query as { email: string, course_code: string };
-    await deleteReview(email, course_code);
-    res.status(200).end();
+    const { email, course_code } = req.query as {
+      email: string
+      course_code: string
+    }
+    await deleteReview(email, course_code)
+    res.status(200).end()
   } catch (err: any) {
-    res.send(`Error: ${err.message}\nDetails: ${err.details}`);
+    res.send(`Error: ${err.message}\nDetails: ${err.details}`)
   }
 }
 
 /**
  * Post a review to the database.
  * @param req A request containing the course review formatted as a JSON in the body.
- * @param res 
+ * @param res
  */
 async function handlePostReview(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const review = req.body as CourseReview;
-    await postReview(review);
-    res.status(200).end();
+    const review = req.body as Course_Review
+    await upsertReview(review)
+    res.status(200).end()
   } catch (err: any) {
-    res.send(`Error: ${err.message}\nDetails: ${err.details}`);
+    res.send(`Error: ${err.message}\nDetails: ${err.details}`)
   }
 }
-
-
