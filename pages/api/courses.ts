2e4087a5c213598fe23e9e66c91c92e37e1edc9e
Maaz Ali <maazali22@gmail.com>
2023-05-27 21:52:59 -0400

    changed courses api to prisma

--- pages/api/courses.ts
+++ pages/api/courses.ts
@@ -1,24 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getAllCourses } from "../../services/course"
 
-import { getAllCourses } from '../../lib/courses';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   switch (req.method) {
-    case 'GET': await getCourses(req, res); break;
-    default: res.send('Invalid API route'); break;
+    case "GET":
+      await getCourses(req, res)
+      break
+    default:
+      res.send("Invalid API route")
+      break
   }
 }
 
 /**
  * Get all courses from the database.
- * @param req 
- * @param res 
+ * @param req The Next.js API request object
+ * @param res The Next.js API response object
  */
 async function getCourses(req: NextApiRequest, res: NextApiResponse) {
   try {
     const courses = await getAllCourses()
-    res.send(courses);
+    res.send(courses)
   } catch (err: any) {
-    res.send(`Error: ${err.message}\nDetails: ${err.details}`);
+    res.send(`Error: ${err.message}\nDetails: ${err.details}`)
   }
 }
