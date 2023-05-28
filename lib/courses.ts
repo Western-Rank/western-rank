4521f26def7b438d8534f67b1c2d2fdf3123637c
Maaz Ali <maazali22@gmail.com>
2023-05-28 15:36:02 -0400

    added fullcoursename type

--- lib/courses.ts
+++ lib/courses.ts
@@ -1,9 +1,17 @@
+import { Course } from "@prisma/client"
+
+export type FullCourseName =
+  `${Course["course_code"]}: ${Course["course_name"]}`
+
 /**
  * Concatenate course code and course name into one entry, e.g. CALC 1000: Calculus I
  * @param courseCode
  * @param courseName
  * @returns The concatenated full course name
  */
-export function formatFullCourseName(courseCode: string, courseName: string) {
+export function formatFullCourseName(
+  courseCode: string,
+  courseName: string,
+): FullCourseName {
   return `${courseCode}: ${courseName}`
 }
