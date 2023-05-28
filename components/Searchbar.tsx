6eb02027e96f1e08507321bdb1e88e2b24cfd5b2
Maaz Ali <maazali22@gmail.com>
2023-05-28 05:37:08 -0400

    formatting

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,46 +1,44 @@
 // import SearchIcon from '@mui/icons-material/Search'
-import { useRouter } from 'next/router';
-import { Autocomplete, TextField, createFilterOptions } from '@mui/material'
-import styles from '../styles/Searchbar.module.scss'
-import { useQuery } from '@tanstack/react-query';
+import { useRouter } from "next/router"
+import { Autocomplete, TextField, createFilterOptions } from "@mui/material"
+import styles from "../styles/Searchbar.module.scss"
+import { useQuery } from "@tanstack/react-query"
 
 const Searchbar = () => {
-  const router = useRouter();
+  const router = useRouter()
   const filterOptions = createFilterOptions({
-    matchFrom: 'any',
-    limit: 15
+    matchFrom: "any",
+    limit: 15,
   })
 
-  const { data, isLoading, isError }= useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryFn: async () => {
-      const response = await fetch('/api/courses');
-      if (!response.ok) {
-        throw new Error('Courses were not found');
-      }
-      return response.json();
+      const response = await fetch("/api/courses")
+      if (!response.ok) throw new Error("Courses were not found")
+
+      return response.json()
     },
     refetchOnWindowFocus: false,
     onSuccess(data) {
-      data.sort();
-    }
-  });
+      data.sort()
+    },
+  })
 
   const onCourseSelect = (ev: React.SyntheticEvent, value: unknown) => {
-    if (typeof(value) !== 'string' || value === null)
-      return;
+    if (typeof value !== "string" || value === null) return
 
     // extract the course code from the full course name,
     // encoding it as a valid URI string
     // e.g. CALC 1000: Calculus I -> CALC%201000
-    const courseCodeURI = encodeURIComponent(value?.split(':')[0]);
-    router.push(`/course/${courseCodeURI}`);
+    const courseCodeURI = encodeURIComponent(value?.split(":")[0])
+    router.push(`/course/${courseCodeURI}`)
   }
 
   return (
     <Autocomplete
       className={styles.searchbar}
       options={!isLoading && !isError ? data : []}
-      renderInput={(params) => <TextField {...params}/>}
+      renderInput={(params) => <TextField {...params} />}
       onChange={onCourseSelect}
       filterOptions={filterOptions}
     />
