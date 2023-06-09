a3c2f9ddf4733c93374500c02a0704b789f909f1
Maaz Ali <maazali22@gmail.com>
2023-06-09 18:56:29 -0400

    saving

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,25 +4,39 @@ import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../lib/db";
 
+// EMAIL_SERVER_USER=maazali22@gmail.com
+// EMAIL_SERVER_PASSWORD=Monster32!2
+// EMAIL_SERVER_HOST=smtp.gmail.com
+// EMAIL_SERVER_PORT=587
+// EMAIL_FROM=maazali22@gmail.com
+
 export const authOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
+      // server: {
+      //   host: process.env.EMAIL_SERVER_HOST,
+      //   port: process.env.EMAIL_SERVER_PORT,
+      //   auth: {
+      //     user: process.env.EMAIL_SERVER_USER,
+      //     pass: process.env.EMAIL_SERVER_PASSWORD,
+      //   },
+      // },
       server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        host: "smtp-relay.sendinblue.com",
+        port: 587,
         auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
+          user: "westernrank@gmail.com",
+          pass: "Pfqv91Lg0WYXJzhK",
         },
       },
-      from: process.env.EMAIL_FROM,
+      from: "westernrank@gmail.com",
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    // }),
   ],
 };
 
