f9decd0f7769dd6732b8573c118a6d1b76f764c0
Maaz Ali <maazali22@gmail.com>
2023-06-10 17:18:18 -0400

    add pages to config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,42 +1,38 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
 import { prisma } from "../../../lib/db";
 
-// EMAIL_SERVER_USER=maazali22@gmail.com
-// EMAIL_SERVER_PASSWORD=Monster32!2
-// EMAIL_SERVER_HOST=smtp.gmail.com
-// EMAIL_SERVER_PORT=587
-// EMAIL_FROM=maazali22@gmail.com
-
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
+  pages: {
+    // signIn: "/auth/signin",
+    // signOut: "/auth/signout",
+    // error: "/auth/error", // Error code passed in query string as ?error=
+    // verifyRequest: "/auth/verify-request", // (used for check email message)
+    // newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   providers: [
     EmailProvider({
-      // server: {
-      //   host: process.env.EMAIL_SERVER_HOST,
-      //   port: process.env.EMAIL_SERVER_PORT,
-      //   auth: {
-      //     user: process.env.EMAIL_SERVER_USER,
-      //     pass: process.env.EMAIL_SERVER_PASSWORD,
-      //   },
-      // },
       server: {
-        host: "smtp-relay.sendinblue.com",
-        port: 587,
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
         auth: {
-          user: "westernrank@gmail.com",
-          pass: "Pfqv91Lg0WYXJzhK",
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
         },
       },
+      // server: {
+      //   host: "smtp-relay.sendinblue.com",
+      //   port: 587,
+      //   auth: {
+      //     user: "westernrank@gmail.com",
+      //     pass: "Pfqv91Lg0WYXJzhK",
+      //   },
+      // },
       from: "westernrank@gmail.com",
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    // }),
   ],
 };
 
