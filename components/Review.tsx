dea0ccea98209bfc738a9d5bc2c939848ff489ff
basokant <basokanthan@gmail.com>
2023-06-15 20:44:22 -0400

    change searchitem, and dot menu styling

--- components/Review.tsx
+++ components/Review.tsx
@@ -139,7 +139,9 @@ export const Review = ({ review, onDelete, onEdit, isUser }: ReviewProps) => {
               ? formatTimeAgo(review.last_edited)
               : formatTimeAgo(review.date_created)}
           </h6>
-          <p className="text-sm flex-grow break-all py-2">{review.review}</p>
+          <p className="text-sm flex-grow break-all py-2">
+            {review.review ?? <span className="text-muted-foreground">[No written review]</span>}
+          </p>
           <h6 className="text-sm">
             {review?.professor && (
               <>
