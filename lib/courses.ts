19298903d869a8dfbf1df7d6a9cc11d08647a6c1
basokant <basokanthan@gmail.com>
2023-06-17 17:40:50 -0400

    add requisite descriptions and popover

--- lib/courses.ts
+++ lib/courses.ts
@@ -4,6 +4,23 @@ import { Course } from "@prisma/client";
 export type FullCourseName = `${Course["course_code"]}: ${Course["course_name"]}`;
 export type FullCourse = Awaited<ReturnType<typeof getCourse>>;
 
+export const requisiteTypes = [
+  "Prerequisites",
+  "Corequisites",
+  "Antirequisites",
+  "Pre-or-Corequisites",
+] as const;
+
+export const requisiteDescription = {
+  Prerequisites:
+    "a course or criteria that must be completed in order to register for this course.",
+  Corequisites: "a course that must be taken concurrently with this course.",
+  Antirequisites:
+    "a course that has very similar content with this course so they cannot both be taken for credit.",
+  "Pre-or-Corequisites":
+    "a course that must be completed before taking or concurrently with this course.",
+};
+
 /**
  * Concatenate course code and course name into one entry, e.g. CALC 1000: Calculus I
  * @param courseCode
