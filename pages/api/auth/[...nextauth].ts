0dc8e1a33e2d3ace606359a82fa4d4dc5e362f02
basokant <basokanthan@gmail.com>
2023-06-18 13:38:50 -0400

    add error page to next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,7 +23,7 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/auth/signin",
     // signOut: "/auth/signout",
-    // error: "/auth/error", // Error code passed in query string as ?error=
+    error: "/auth/error", // Error code passed in query string as ?error=
     verifyRequest: "/auth/verify", // (used for check email message)
     // newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
