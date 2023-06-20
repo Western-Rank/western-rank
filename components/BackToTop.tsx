2ec89db3e1981242573e77b47e258f775d35f4a7
Maaz Ali <maazali22@gmail.com>
2023-06-20 17:44:36 -0400

    added svg

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -17,7 +17,26 @@ const BackToTop = ({ offset = 100 }: BackToTopProps) => {
 
   if (!hasScrolled) return null;
 
-  return <Button className="fixed bottom-3 right-3"></Button>;
+  return (
+    <Button className="fixed bottom-3 right-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="lucide lucide-arrow-up-to-line"
+      >
+        <path d="M5 3h14" />
+        <path d="m18 13-6-6-6 6" />
+        <path d="M12 7v14" />
+      </svg>
+    </Button>
+  );
 };
 
 export default BackToTop;
