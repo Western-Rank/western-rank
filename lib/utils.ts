8374851b1c82f290e51514a099817acae60d175c
basokant <basokanthan@gmail.com>
2023-06-29 16:50:51 -0400

    check if the user is active in any input element for / key shortcut

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,16 @@ export function isUwoEmail(email: string) {
   return email.endsWith("@uwo.ca") || email.endsWith("@ivey.ca");
 }
 
+export function hasActiveInput() {
+  var activeElement = document.activeElement;
+  var inputs = ["input", "select", "button", "textarea"];
+
+  if (activeElement && inputs.indexOf(activeElement.tagName.toLowerCase()) !== -1) {
+    return true;
+  }
+  return false;
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
